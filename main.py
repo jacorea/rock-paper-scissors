45f58608bc11107774376acf68ef3a7c21af4d46
jacorea <45439228+jacorea@users.noreply.github.com>
2023-10-10 23:08:53 +0000

    added invalid input condition

--- main.py
+++ main.py
@@ -28,27 +28,31 @@ scissors = '''
 #Write your code below this line 👇
 symbols = [rock, paper, scissors]
 import random
-computer = str(random.randint(0,2))
-print(computer)
-player = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
 
-print(symbols[int(player)])
-print('computer chose:')
-print(symbols[int(computer)])
+computer = str(random.randint(0, 2))
+player = input(
+    "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+
+# Invalid input
+if int(player) >= 3 or int(player) < 0:
+  print('you chose an invalid input, you lose!')
+else:
+  print(symbols[int(player)])
+  print('computer chose:')
+  print(symbols[int(computer)])
 
 # draw
 if computer == player:
-  print("It's a draw!")
+    print("It's a draw!")
 
-# combo 
-combo = player+computer
+# combo
+combo = player + computer
 
-# make winning list 
-win_combos = ['02','10','21']
+# make winning list
+win_combos = ['02', '10', '21']
 
-# check wins 
+# check wins
 if combo == win_combos[0] or combo == win_combos[1] or combo == win_combos[2]:
-  print('you win!')
+    print('you win!')
 else:
-  print('you lose!')
-
+    print('you lose!')
